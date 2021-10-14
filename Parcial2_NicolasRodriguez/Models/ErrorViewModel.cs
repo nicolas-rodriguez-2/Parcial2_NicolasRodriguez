4c12f4fd9503b1ad54fff3e4a3335732dd23efe8
nicolas-rodriguez-2 <a2018121014@estudiantes.upsa.edu.bo>
2021-10-13 21:01:00 -0400

    Proyecto creado

---
+++ Parcial2_NicolasRodriguez/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Parcial2_NicolasRodriguez.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
