56e1752cd9f0c34b60c38a563ce37bcf2701a3ea
nicolas-rodriguez-2 <a2018121014@estudiantes.upsa.edu.bo>
2021-10-13 22:00:48 -0400

    API agregada y comprobada con Postman

---
+++ Parcial2_NicolasRodriguez/Controllers/ApiFriendsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Parcial2_NicolasRodriguez.Data;
+using Parcial2_NicolasRodriguez.Models;
+
+namespace Parcial2_NicolasRodriguez.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiFriendsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ApiFriendsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ApiFriends
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Friend>>> GetFriend()
+        {
+            return await _context.Friend.ToListAsync();
+        }
+
+        // GET: api/ApiFriends/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Friend>> GetFriend(int id)
+        {
+            var friend = await _context.Friend.FindAsync(id);
+
+            if (friend == null)
+            {
+                return NotFound();
+            }
+
+            return friend;
+        }
+
+        // PUT: api/ApiFriends/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFriend(int id, Friend friend)
+        {
+            if (id != friend.FriendId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(friend).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FriendExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ApiFriends
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Friend>> PostFriend(Friend friend)
+        {
+            _context.Friend.Add(friend);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFriend", new { id = friend.FriendId }, friend);
+        }
+
+        // DELETE: api/ApiFriends/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFriend(int id)
+        {
+            var friend = await _context.Friend.FindAsync(id);
+            if (friend == null)
+            {
+                return NotFound();
+            }
+
+            _context.Friend.Remove(friend);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FriendExists(int id)
+        {
+            return _context.Friend.Any(e => e.FriendId == id);
+        }
+    }
+}
