a0b80bfdd45f65adbb6cbab534b3cf18a2a7d2ce
nicolas-rodriguez-2 <a2018121014@estudiantes.upsa.edu.bo>
2021-10-13 21:04:35 -0400

    Modelo Friend añadido

---
+++ Parcial2_NicolasRodriguez/Models/Friend.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2_NicolasRodriguez.Models
+{
+    public class Friend
+    {
+        [Key]
+        public int FriendId { get; set; }
+
+
+
+        [Required] //primero las condiciones
+        [Display(Name = "Nombre")]
+        public string Name { get; set; } //luego se muestra que chota
+
+
+
+        [Required]
+        [Display(Name = "Nickname")]
+        public string Nickname { get; set; }
+
+        [Required]
+        [Display(Name = "Enlace")]
+        public DataType Birthday { get; set; }
+
+    }
+}
