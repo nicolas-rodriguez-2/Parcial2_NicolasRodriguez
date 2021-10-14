ae4dac85092ac340edb17050b37e56e387d232f4
nicolas-rodriguez-2 <a2018121014@estudiantes.upsa.edu.bo>
2021-10-13 21:12:47 -0400

    Cadena de conexión añadida al Startup.cs

--- Parcial2_NicolasRodriguez/Startup.cs
+++ Parcial2_NicolasRodriguez/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Parcial2_NicolasRodriguez.Data;
 
 namespace Parcial2_NicolasRodriguez
 {
@@ -24,6 +26,10 @@ namespace Parcial2_NicolasRodriguez
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+
+            services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("DbContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
